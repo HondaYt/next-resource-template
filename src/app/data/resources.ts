3e23b3708e68afb5823cf41f0e0e9f639a989176
HondaYt <honda.yuto.04@gmail.com>
2024-12-02 00:03:34 +0900

    完成

---
+++ src/app/data/resources.ts
@@ -0,0 +1,14 @@
+export const resources = [
+	{
+		title: "🚀 tests",
+		href: "tests",
+		links: [
+			{
+				href: "test01",
+				title: "test01",
+				description: "this is test page.",
+				date: "20XX/XX/XX",
+			},
+		],
+	},
+];
---
+++ src/app/tests/test01/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Template() {
+	return (
+		<main>
+			<h1>Test Page!!!</h1>
+		</main>
+	);
+}
